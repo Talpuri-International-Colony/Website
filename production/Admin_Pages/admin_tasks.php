a595d64118686bb0e5912f7983f2bbcb9b07c096
Ashish Ekka <ashishekka329@gmail.com>
2017-09-26 19:43:31 +0530

    Notice Pins Added
    We can now add new notice to the database

--- production/Admin_Pages/admin_tasks.php
+++ production/Admin_Pages/admin_tasks.php
@@ -465,6 +465,7 @@
 
 
                                                     <div class="col-md-12 col-sm-6 col-xs-12">
+
                                                         <div class="x_panel">
                                                           <div class="x_title">
                                                             <h2> Here is a list of all Unresolved Complaints <small> Respond to them by taking action </small></h2>
@@ -548,6 +549,94 @@
                                         </div>
                                         <div role="tabpanel" class="tab-pane fade" id="tab_content3" aria-labelledby="profile-tab">
                                           <p> Under construction </p>
+
+
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Add A New Notice</h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="#">Settings 1</a>
+                          </li>
+                          <li><a href="#">Settings 2</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+
+                    <!-- start form for validation -->
+                    <form id="add_notice" data-parsley-validate>
+
+                      <label for="noticedate">Notice Date* :</label>
+                      <input type="Date" id="noticedate" class="form-control" name="noticedate" required />
+
+                      <label for="noticetitle">Notice Title * :</label>
+                      <input type="text" id="noticetitle" class="form-control" name="noticetitle" required />
+
+                          <label for="message">Notice Content (20 chars min, 100 max) :</label>
+                          <textarea id="message" required="required" class="form-control" name="message" data-parsley-trigger="keyup" data-parsley-minlength="20" data-parsley-maxlength="100" data-parsley-minlength-message="Come on! You need to enter at least a 20 caracters long comment.."
+                            data-parsley-validation-threshold="10"></textarea>
+
+                          <br/>
+                          <a class="btn btn-default" onclick="add_notice()">Add Notice</a>
+                          <!-- <span class="btn btn-primary" onclick="add_notice()">Add Notice</span> -->
+
+                    </form>
+                    <!-- end form for validations -->
+
+                  </div>
+                </div>
+
+                <script type="text/javascript">
+
+                      var add_notice = function()
+                      {
+                        var entered_date = $('#noticedate').val();
+                        var entered_title = $('#noticetitle').val();
+                        var entered_msg = $('#message').val();
+
+                        $.ajaxSetup({
+                            headers: {
+                              'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                            }
+                          });
+
+                        slash_last_indx = window.location.href.lastIndexOf('/')
+
+                        //window.alert(window.location.href.substring(0,slash_last_indx) + '/VR_scripts/helper_modules.php');
+
+                         $.ajax({
+                                   type: "POST",
+                                   //CSRF: getCSRFTokenValue(),
+                                   //url:  'VR_scripts/helper_modules.php',
+                                   url : window.location.href.substring(0,slash_last_indx) + '/../VR_scripts/helper_modules.php',
+                                   data: {action:'add_notice', _not_date_ : entered_date, _not_title_ : entered_title, _not_msg_ : entered_msg},
+                                   success:function(return_data) {
+                                    new PNotify({
+                                      text: 'Success !',
+                                      text: 'Successfully added new notice!',
+                                      type: 'success',
+                                      styling: 'bootstrap3'
+                                    });
+                                    window.setTimeout(function(){location.reload()},5000);
+                                   }
+
+                              });
+
+
+                      }
+
+                  </script>
+
                                         </div>
                                       </div>
                                     </div>
